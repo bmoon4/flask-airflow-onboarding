043f8aaace930ffa4e2b0d439a7340973e2c274d
Bokyung Moon <bkmoon0702@gmail.com>
2022-06-10 19:16:58 -0400

    script update

--- script.sh
+++ script.sh
@@ -4,9 +4,7 @@
 appcode=$1
 email=$2
 
-echo "your appcode: " $appcode
-echo "your email: " $email
-#set variables
+#set global variables
 lp_prefix="APP_"
 lp_suffix_user="_USER"
 lp_suffix_viewer="_VIEWER"
@@ -28,81 +26,63 @@ rm -rf temp
 # git setup
 git config --global user.email "bkmoon0702@gmail.com"
 
-echo "############################# LDAP setup ###############################"
 
+ldap_change(){
+    #add new change
+    echo "add changes"
+    echo "  - "$lp_user >> configs/dev/ldap.yaml
+    echo "  - "$lp_user >> configs/qa/ldap.yaml
+    echo "  - "$lp_user >> configs/uat/ldap.yaml
+    echo "  - "$lp_viewer >> configs/prod/ldap.yaml
+}
 
-# git clone
-mkdir temp
-echo "git clone"
-git clone https://XXXXX@github.com/bmoon4/ldap-test.git temp
-cd temp
+pool_change(){
+    #add new change
+    echo "add changes"
+    echo $pool >> configs/dev/pools.yaml
+    echo $pool >> configs/qa/pools.yaml
+    echo $pool >> configs/uat/pools.yaml
+    echo $pool >> configs/prod/pools.yaml
+}
 
-# git checkout -b onboarding-$appcode
-echo "git checkout -b"
-git checkout -b $new_branch
+add_new_configs(){
+    type=$1
 
-#add new change
-echo "add changes"
-echo "  - "$lp_user >> configs/dev/ldap.yaml
-echo "  - "$lp_user >> configs/qa/ldap.yaml
-echo "  - "$lp_user >> configs/uat/ldap.yaml
-echo "  - "$lp_viewer >> configs/prod/ldap.yaml
+    echo "############################# $type setup ###############################"
 
-echo "git add ."
-git add .
+    # git clone
+    mkdir temp
 
-# git commit
+    echo "git clone"
+    git clone https://XXXXX@github.com/bmoon4/$type-test.git temp
 
-echo "git commit"
-git commit -m "[ldap] new appcode onboarding"
+    cd temp
 
-# git push
-git push -u --set-upstream origin $new_branch
+    # git checkout -b onboarding-$appcode
+    echo "git checkout -b"
+    git checkout -b $new_branch
 
-# git pull request
-git request-pull origin/main https://github.com/bmoon4/ldap-test $new_branch
+    if [[ $type == "ldap" ]]; then
+       ldap_change
+    else
+       pool_change
+    fi
 
-# Opening PR link : https://github.com/bmoon4/ldap-test/pull/new/${new_branch}
+    # git add .
+    git add .
 
-# clean up temp folder
-rm -rf temp
+    # git commit
+    git commit -m "[ldap] new appcode onboarding"
 
+    # git push
+    git push -u --set-upstream origin $new_branch
 
-echo "############################# Pool setup ###############################"
+    # git pull request
+    git request-pull origin/main https://github.com/bmoon4/$type-test $new_branch
 
-# git clone
-mkdir temp
-echo "git clone"
-git clone https://XXXXX@github.com/bmoon4/pool-test.git temp
-cd temp
+    # clean up temp folder
+    rm -rf temp
+}
 
-# git checkout -b onboarding-$appcode
-echo "git checkout -b"
-git checkout -b $new_branch
-
-#add new change
-echo "add changes"
-echo $pool >> configs/dev/pools.yaml
-echo $pool >> configs/qa/pools.yaml
-echo $pool >> configs/uat/pools.yaml
-echo $pool >> configs/prod/pools.yaml
-echo "git add ."
-git add .
-
-# git commit
-
-echo "git commit"
-git commit -m "[pool] new appcode onboarding"
-
-# git push
-git push -u --set-upstream origin $new_branch
-
-# git pull request
-git request-pull origin/main https://github.com/bmoon4/pool-test $new_branch
-
-# Opening PR link : https://github.com/bmoon4/pool-test/pull/new/${new_branch}
-
-# clean up temp folder
-rm -rf temp
-
-exit 0
+add_new_configs ldap
+add_new_configs pool
